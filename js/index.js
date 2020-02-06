c91573e452d3ef9104dd8975beb664ede39e956d
Sahil1602 <sb160295@gmail.com>
2020-02-06 16:59:01 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,109 @@
+$(Document).ready(function(){
+    $(".carousel").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+
+    })
+})
+
+// {/* <div class="cards">
+//     <img src="https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg"/>
+//     <h4>Men Navy Blue Solid Sweatshirt</h4>
+//     <p>United Colors of Benetton</p>
+//     <p>Rs 2599</p>
+// </div> */}
+
+function createClothesCard(obj){
+    var clothCardWrap = document.createElement('div');
+    clothCardWrap.className = 'cards';
+
+    var clothImage = document.createElement('img');
+    clothImage.src = obj.preview;
+    clothCardWrap.appendChild(clothImage);
+
+    var clothName = document.createElement('h4');
+    clothName.innerHTML = obj.name;
+    clothName.className = 'product-name';
+    clothCardWrap.appendChild(clothName);
+
+    var clothBrandName = document.createElement('p');
+    clothBrandName.innerHTML = obj.brand;
+    clothBrandName.className = 'brand';
+    clothCardWrap.appendChild(clothBrandName);
+
+    var clothPriceTag = document.createElement('p');
+    clothPriceTag.innerHTML = 'Rs.' + obj.price;
+    clothPriceTag.className = 'price';
+    clothCardWrap.appendChild(clothPriceTag);
+
+    clothCardWrap.onclick = function() {
+        localStorage.setItem('cardId', obj.id);
+        console.log(obj.id);
+        location.assign("product_details.htm");
+    }
+
+
+    return clothCardWrap;
+
+
+}
+
+function createAccessoriesCard(obj){
+    var accessoryCardWrap = document.createElement('div');
+    accessoryCardWrap.className = 'cards';
+
+    var accessoryImage = document.createElement('img');
+    accessoryImage.src = obj.preview;
+    accessoryCardWrap.appendChild(accessoryImage);
+
+    var accessoryName = document.createElement('h4');
+    accessoryName.innerHTML = obj.name;
+    accessoryName.className = 'product-name';
+    accessoryCardWrap.appendChild(accessoryName);
+
+    var accessoryBrandName = document.createElement('p');
+    accessoryBrandName.innerHTML = obj.brand;
+    accessoryBrandName.className = 'brand';
+    accessoryCardWrap.appendChild(accessoryBrandName);
+
+    var accessoryPriceTag = document.createElement('p');
+    accessoryPriceTag.innerHTML = 'Rs.' + obj.price;
+    accessoryPriceTag.className = 'price';
+    accessoryCardWrap.appendChild(accessoryPriceTag);
+
+    accessoryCardWrap.onclick = function() {
+        localStorage.setItem('cardId', obj.id);
+        console.log(obj.id);
+        location.assign("product_details.htm");
+    }
+    
+
+    return accessoryCardWrap;
+
+
+}
+
+
+var clothCardSection = document.getElementById('cloth-cards-wrap');
+var accessoreyCardSection = document.getElementById('access-cards-wrap');
+
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://5d76bf96515d1a0014085cf9.mockapi.io/product/', true);
+xhttp.onreadystatechange = function() {
+    if(this.readyState === 4){
+        var ecomData = JSON.parse(this.responseText);
+        for(var i=0; i<ecomData.length; i++){
+            if(ecomData[i].isAccessory == false){
+                clothCardSection.appendChild(createClothesCard(ecomData[i]));
+            }
+            else{
+                accessoreyCardSection.appendChild(createAccessoriesCard(ecomData[i]));
+            }
+        }
+    }
+}
+
+xhttp.send()
+
---
+++ js/product_details.js
@@ -0,0 +1,135 @@
+
+
+
+{/* 
+<img id="display-pic" src="https://i.imgur.com/rlm5PWt.jpg" alt="shirt"/>
+<div id="detail-wrap">
+    <h1>Men Navy Blue Solid Sweat Shirt</h1>
+    <p>United Colors of Benetton</p>
+    <p>
+        <span>Price: Rs </span>  <span>2599</span>
+    </p>
+    <h4>Description</h4>
+    <p>Navy solid sweatshirt with patchwork, has a round neck,<br> long sleeves, straight hem</p>
+    <h4>Product Preview</h4>
+    <div id="product-preview-wrap">
+        <div>
+            <img class="preview-card" src="https://i.imgur.com/rlm5PWt.jpg"/>
+        </div>
+        
+    </div>
+    <button>Add to Cart</button>
+</div> */}
+
+
+
+function createProductDetailPage(obj){
+    var page2Wrap = document.createElement('div');
+    page2Wrap.className = "wrapper";
+
+    var displayPic = document.createElement('img');
+    displayPic.src = obj.preview;
+    displayPic.className = "displayImg";
+    page2Wrap.appendChild(displayPic);
+
+    var detailWrap = document.createElement('div');
+    detailWrap.id = "detail-wrap";
+    page2Wrap.appendChild(detailWrap);
+
+    var productTitle = document.createElement("h1");
+    productTitle.innerHTML = obj.name;
+    productTitle.id = "product-title";
+    detailWrap.appendChild(productTitle)
+
+    var brandName = document.createElement("p");
+    brandName.innerHTML = obj.brand;
+    brandName.id = "brand-name";
+    detailWrap.appendChild(brandName);
+
+    var priceTag = document.createElement("p");
+    detailWrap.appendChild(priceTag);
+
+    var priceCons = document.createElement("span");
+    priceCons.innerHTML = "Price: Rs ";
+    priceTag.appendChild(priceCons);
+    
+    var priceVar = document.createElement("span");
+    priceVar.innerHTML = obj.price;
+    priceTag.appendChild(priceVar);
+    priceVar.id = "real-price";
+
+    var descriptionTxt = document.createElement("h4");
+    descriptionTxt.innerHTML = "Description";
+    descriptionTxt.className = "sub-title";
+    detailWrap.appendChild(descriptionTxt)
+
+    var fullDescription = document.createElement("p");
+    fullDescription.innerHTML= obj.description;
+    fullDescription.id = "description";
+    detailWrap.appendChild(fullDescription);
+    
+    var productPreview = document.createElement("h4");
+    productPreview.innerHTML = "Product Preview";
+    productPreview.className = "sub-title";
+    detailWrap.appendChild(productPreview);
+
+    var previewPhotosWrap = document.createElement('div');
+    previewPhotosWrap.className = "product-preview-wrap";
+
+    for(var j=0;j<obj.photos.length;j++) {
+        var picWrap = document.createElement('div');
+        // picWrap.className = "pic-wrap";
+        var previewphoto = document.createElement('img');
+        previewphoto.className = "preview-card";
+        previewphoto.src = obj.photos[j];
+        // picWrap.appendChild(previewphoto);
+
+        previewPhotosWrap.appendChild(previewphoto);
+
+        
+    }
+
+
+    previewPhotosWrap.onclick = function(e){
+        if(e.target.tagName === 'IMG'){
+            displayPic.src = e.target.src;
+            // e.target.style.border = "solid #2198e6 2px";
+            console.log(previewphoto);
+            var parentDiv = e.target.parentElement;
+            for(let i=0; i < parentDiv.children.length; i++){
+    	        parentDiv.children[i].classList.remove('selected');
+            }
+            e.target.classList.add('selected');
+        }
+        
+    }
+    
+    detailWrap.appendChild(previewPhotosWrap);
+
+    var addToCart = document.createElement('button');
+    addToCart.innerHTML = 'Add to Cart';
+    detailWrap.appendChild(addToCart);
+
+    return page2Wrap;
+    
+}
+
+var secondPageMain = document.getElementById("main-div-page2");
+
+var xhttp = new XMLHttpRequest();
+xhttp.open('Get', 'https://5d76bf96515d1a0014085cf9.mockapi.io/product', true);
+xhttp.onreadystatechange = function() {
+    if(this.readyState === 4) {
+        var ecomData = JSON.parse(this.responseText);
+        for(var i=0; i<ecomData.length; i++) {
+            if(ecomData[i].id === localStorage.getItem("cardId"))
+                secondPageMain.appendChild(createProductDetailPage(ecomData[i]));
+        }
+    }
+}
+xhttp.send();
+
+
+
+
+
